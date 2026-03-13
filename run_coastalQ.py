4bc63775a57ff11ce6bbae0190f807ebe7efe7b3
wrightky <kyleawright@utexas.edu>
2026-03-13 11:59:25 +0100

    initialize runscript

---
+++ run_coastalQ.py
@@ -0,0 +1,15 @@
+"""Script to run coastalQ module."""
+
+# Standard imports
+import numpy as np
+import os
+from netCDF4 import Dataset
+
+# Local imports
+from coastalQ import DeltaPartition
+
+# def main():
+#     """Main method to execute coastalQ class methods."""
+
+# if __name__ == "__main__":
+#     main()
